451801ffc7e8f5c8dbfbd307ee4746ff905e99ed
zerozzl <444056035@qq.com>
2016-10-10 22:44:27 +0800

    update Basic Usage

--- Basic_Usage.py
+++ Basic_Usage.py
@@ -48,3 +48,37 @@ sub = tf.sub(x, a)
 print sub.eval()
 isess.close()
 '''
+
+# Variables
+state = tf.Variable(0, name="counter")
+one = tf.constant(1)
+new_value = tf.add(state, one)
+update = tf.assign(state, new_value)
+
+init_op = tf.initialize_all_variables()
+with tf.Session() as sess:
+    sess.run(init_op)
+    print sess.run(state)
+
+    for _ in range(3):
+        sess.run(update)
+        print sess.run(state)
+
+# Fetches
+input1 = tf.constant([3.])
+input2 = tf.constant([2.])
+input3 = tf.constant([5.])
+intermed = tf.add(input2, input3)
+mul = tf.mul(input1, intermed)
+
+with tf.Session() as sess:
+    result = sess.run([mul, intermed])
+    print result
+
+# Feeds
+input1 = tf.placeholder(tf.float32)
+input2 = tf.placeholder(tf.float32)
+output = tf.mul(input1, input2)
+
+with tf.Session() as sess:
+    print sess.run([output], feed_dict={input1: [7.], input2: [2.]})
